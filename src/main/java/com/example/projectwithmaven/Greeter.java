9f308097399e2ca00b9681de2c333b241294336b
alaryushkin <alexandr_94lion@mail.ru>
2021-10-06 23:17:21 +0300

    Implemented the manual https://spring.io/guides/gs/maven

---
+++ src/main/java/com/example/projectwithmaven/Greeter.java
@@ -0,0 +1,7 @@
+package com.example.projectwithmaven;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/projectwithmaven/GreeterTest.java
@@ -0,0 +1,17 @@
+package projectwithmaven;
+
+import com.example.projectwithmaven.Greeter;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+public class GreeterTest {
+
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+    }
+}
